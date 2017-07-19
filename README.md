a44269469262bcf43856dd3f359486305955787d
vaszol <vaszol@mail.ru>
2017-07-19 18:50:58 +0300

    Решение задачи из первого урока.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# LoaderWeather" 
+
+README
+
+LoaderWeather
+
+Пример приложения для загрузки и отображения погоды.
+
+Ссылка на урок:
+http://www.fandroid.info/lektsiya-1-vvedenie-v-arhitekturu-klient-servernyh-android-prilozhenij-chast-2/
+
+
--- app/src/main/java/ru/gdgkazan/simpleweather/screen/weatherlist/WeatherListActivity.java
+++ app/src/main/java/ru/gdgkazan/simpleweather/screen/weatherlist/WeatherListActivity.java
@@ -1,8 +1,14 @@
 package ru.gdgkazan.simpleweather.screen.weatherlist;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +16,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import butterknife.BindView;
@@ -20,11 +28,12 @@ import ru.gdgkazan.simpleweather.screen.general.LoadingDialog;
 import ru.gdgkazan.simpleweather.screen.general.LoadingView;
 import ru.gdgkazan.simpleweather.screen.general.SimpleDividerItemDecoration;
 import ru.gdgkazan.simpleweather.screen.weather.WeatherActivity;
+import ru.gdgkazan.simpleweather.screen.weather.WeatherLoader;
 
 /**
  * @author Artur Vasilov
  */
-public class WeatherListActivity extends AppCompatActivity implements CitiesAdapter.OnItemClick {
+public class WeatherListActivity extends AppCompatActivity implements CitiesAdapter.OnItemClick, SwipeRefreshLayout.OnRefreshListener {
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
@@ -39,6 +48,13 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
 
     private LoadingView mLoadingView;
 
+    //Список городов с загруженной инфой о погоде
+    private List<City> loadedCity = new ArrayList<>();
+    private List<City> cities;
+
+    @BindView(R.id.swipeContainer)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,21 +68,70 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
         mRecyclerView.setAdapter(mAdapter);
         mLoadingView = LoadingDialog.view(getSupportFragmentManager());
 
-        /**
-         * TODO : task
-         *
-         * 1) Load all cities forecast using one or multiple loaders
-         * 2) Try to run these requests as most parallel as possible
-         * or better do as less requests as possible
-         * 3) Show loading indicator during loading process
-         * 4) Allow to update forecasts with SwipeRefreshLayout
-         * 5) Handle configuration changes
-         *
-         * Note that for the start point you only have cities names, not ids,
-         * so you can't load multiple cities in one request.
-         *
-         * But you should think how to manage this case. I suggest you to start from reading docs mindfully.
-         */
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeColors(Color.RED, Color.GREEN, Color.BLUE, Color.CYAN);
+
+        cities = getInitialCities();
+
+        load(cities, false);
+    }
+
+    @Override
+    public void onRefresh() {
+        load(cities, true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        }, 4000);
+    }
+
+    private class WeatherCallbacks implements LoaderManager.LoaderCallbacks<City> {
+
+        private City city;
+        private String cityName;
+
+        public WeatherCallbacks(City city, String cityName) {
+            this.city = city;
+            this.cityName = cityName;
+        }
+
+        @Override
+        public Loader<City> onCreateLoader(int id, Bundle args) {
+            //создаем для каждого города Loader
+            if (id <= cities.size()) {
+                return new WeatherLoader(WeatherListActivity.this, cityName);
+            }
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<City> loader, City city) {
+            showWather(city);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<City> loader) {
+
+        }
+    }
+
+    private void loadWeather(boolean restart, City city, String cityName, Integer id) {
+        mLoadingView.showLoadingIndicator();
+        LoaderManager.LoaderCallbacks<City> callbacks = new WeatherCallbacks(city, cityName);
+        if (restart) {
+            getSupportLoaderManager().restartLoader(id, Bundle.EMPTY, callbacks);
+        } else {
+            getSupportLoaderManager().initLoader(id, Bundle.EMPTY, callbacks);
+        }
+    }
+
+    private void load(List<City> cities, boolean restart) {
+        for (int i = 0; i < cities.size(); i++) {
+            String cityName = cities.get(i).getName();
+            loadWeather(restart, cities.get(i), cityName, i + 1);
+        }
     }
 
     @Override
@@ -82,5 +147,38 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
             cities.add(new City(city));
         }
         return cities;
+    }
+
+    private List<City> sortAllCities(List<City> cities) {
+        Collections.sort(cities, new Comparator<City>() {
+            @Override
+            public int compare(City t0, City t1) {
+                return t0.getName().compareTo(t1.getName());
+            }
+        });
+        return cities;
+    }
+
+    private void showError() {
+        mLoadingView.hideLoadingIndicator();
+        Snackbar snackbar = Snackbar.make(mRecyclerView, "Error loading weather", Snackbar.LENGTH_LONG)
+                .setAction("Retry", v -> load(cities, true));
+        snackbar.setDuration(4000);
+        snackbar.show();
+    }
+
+    private void showWather(@Nullable City city) {
+        if (city == null || city.getMain() == null || city.getWeather() == null || city.getWind() == null) {
+            showError();
+            return;
+        }
+
+        loadedCity.add(city);
+        if (loadedCity.size() >= cities.size()) {
+            mLoadingView.hideLoadingIndicator();
+            sortAllCities(loadedCity);
+            mAdapter.changeDataSet(loadedCity);
+            loadedCity.clear();
+        }
     }
 }
